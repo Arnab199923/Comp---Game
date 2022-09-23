1cb779f72ad50d2c6b09c1a498a2b6a6f2129356
Arnab199923 <96194042+Arnab199923@users.noreply.github.com>
2022-09-23 06:00:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+// // var food = prompt("amount of food")
+// // var foodtip = prompt("tip amount")
+// // var tax = prompt("enter tax")*12
+// // var calculate = food*foodtip+tax
+// // alert(calculate) 
+
+
+
+// var text = prompt("try typing hi,hello,how are you and open browser on your keyboard");
+
+// if(text=="hi"){
+//     alert("hello");
+// }else if (text=="hello") {
+//     alert("welcome i am your chat bot try reloding me");
+// } else if(text=="how are you"){
+//     alert("i will be fine if cpu takes less load");
+// }else if(text=="open browser"){
+//     alert("sorry this developer is not a pro coder")
+// }else{
+//     alert("something went wrong try reloding me");
+// }
+
+
+
+var rock = 0;
+var paper = 1;
+var sicsor = 3;
+var count =1;
+var computer=0;
+var player = 0;
+while(count>0){
+
+
+var comp = Math.floor(Math.random()*3);
+var you = prompt("enter your choice number for rock : 0, for paper : 1, for scissor :2");
+var count=10;
+
+
+
+    if(comp==you){
+        alert("your score"+" "+player++);
+    }else if(comp!=you){
+        alert("computer score"+" "+computer++);
+    }else{
+        alert("wrong input provided");
+    }
+count++;
+if(computer>5|| player>5){
+    alert("comouter score"+" "+computer+" "+" "+"player score"+" "+player);
+    alert("game ended reload for play again")
+    break;
+    
+}
+}   
+
